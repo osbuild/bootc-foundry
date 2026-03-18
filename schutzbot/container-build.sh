8e9278579b6ccd6108a9f99adb963dbcae82f31e
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2026-03-18 17:38:05 +0100

    .gitlab-ci.yml: test builds in which uses the builder container
    Test the (nested) builds in an unprivileged container. The container
    builds cs9, cs10, rhel-9.7 and rhel-10.1.

---
+++ schutzbot/container-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -euo pipefail
+CONTAINER=$(podman build -f distribution/Dockerfile-ubi . | tail -n 1)
+podman run -e PYTHONUNBUFFERED=1 -e RH_CREDS="${RH_CREDS:-}" "$CONTAINER" --repo placeholder.org --distro "$1"
