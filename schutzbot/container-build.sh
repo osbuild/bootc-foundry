78a452183e3b92412f77923ef0b8af1e4372308e
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2026-03-24 11:12:44 +0100

    distribution: create small entrypoint which just builds one
    The cronjob is failing and there's a lack of output. I think
    subprocess.run might be swallowing some output somehow.

--- schutzbot/container-build.sh
+++ schutzbot/container-build.sh
@@ -2,4 +2,5 @@
 
 set -euo pipefail
 CONTAINER=$(podman build -f distribution/Dockerfile-ubi . | tail -n 1)
-podman run -e PYTHONUNBUFFERED=1 -e RH_CREDS="${RH_CREDS:-}" "$CONTAINER" --repo placeholder.org --distro "$1"
+# podman run -e PYTHONUNBUFFERED=1 -e RH_CREDS="${RH_CREDS:-}" "$CONTAINER" --repo placeholder.org --distro "$1"
+podman run "$CONTAINER"
