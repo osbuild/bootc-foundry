6e5618622b94c6a3ae909acc262b2a4d222437e4
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2026-03-25 14:42:01 +0100

    many: revert openshift work
    The approach of building the containers in an openshift cronjob will not
    work. Even with the chroot isolation mechanism the underlying OS is
    locked down to not allow unsharing the namespace call: `Error during
    unshare(CLONE_NEWUSER): Operation not permitted`.
    
    The best option now is to build everything within konflux. This reverts
    the following commits:
    
    - distribution/Dockerfile-ubi: set up working directory properly
      This reverts commit 49acdfb1ae2eee015603c26621debffe18e8d8d4
    - schutzbot/build: create temporary containerfile in /tmp
      This reverts commit 430be78eead3e2822105538fc24f7e7b4556e408
    - schutzbot/gitlab-utils: fix trace_quiet/restore
      This reverts commit d8cac35ae929e9b951a17a601ab405cab8df2893
    - schutzbot/build: write Containerfile step by step
      This reverts commit efd222d2ce48ff1567b3e55ead1f83dbdab1e69b
    - templates/openshift: increase cronjob frequency
      This reverts commit 7f66a8ac1884800a151e6670e8e09683db5b128e
    - distribution: create small entrypoint which just builds one
      This reverts commit 8584429e413889dc6deddca7db8881f52cf051e9
    - templates/openshift: add containers cronjob
      This reverts commit 4bcf2cd8ebfa4ae42aad4b260a7e8d8fc2f5c3db
    - schutzbot/build: use force copy on temporary containerfile
      This reverts commit b65810001c2e37194f1dbf06aa1c2a0225e4e310
    - gitlab-ciyml: test builds in which uses the builder container
      This reverts commit 8e9278579b6ccd6108a9f99adb963dbcae82f31e
    - distribution/Dockerfile-ubi: add containerfile for bootc-foundry
      This reverts commit 8810befa03f104a07cfe9d56ac14b5e33c7ebbaf

--- schutzbot/gitlab-utils.sh
+++ schutzbot/gitlab-utils.sh
@@ -6,15 +6,12 @@ set -euo pipefail
 
 # Hide "set +x"/"set -x" from trace when possible (BASH_XTRACEFD).
 _trace_quiet() {
-    if [[ $- == *x* ]]; then
-        set +x
-        echo -n 1
-    fi
-    echo -n 0
+    exec 3>/dev/null 2>/dev/null || true
+    BASH_XTRACEFD=3 set +x 2>/dev/null || set +x
 }
 
 _trace_restore() {
-    set -x
+    BASH_XTRACEFD=3 set -x 2>/dev/null || set -x
 }
 
 # Prints a section start message to the console.
@@ -27,8 +24,7 @@ _trace_restore() {
 # Example:
 #   section_start build_image "Building image"
 function section_start() {
-    local restore_trace
-    restore_trace=$(_trace_quiet)
+    _trace_quiet
     local section_id="${1}_$$"
     local section_title=$2
     local collapsed=${3:-true}
@@ -36,9 +32,7 @@ function section_start() {
     [ "$collapsed" == "true" ] && params="[collapsed=true]"
 
     printf "\e[0Ksection_start:%s:%s%s\r\e[0K\e[1;36m%s\e[0m\n" "$(date +%s)" "$section_id" "$params" "$section_title"
-    if [ "$restore_trace" == 1 ]; then
-       _trace_restore
-    fi
+    _trace_restore
 }
 
 # Prints a section end message to the console.
@@ -49,11 +43,8 @@ function section_start() {
 # Example:
 #   section_end build_image
 function section_end() {
-    local restore_trace
-    restore_trace=$(_trace_quiet)
+    _trace_quiet
     local section_id="${1}_$$"
     printf "\e[0Ksection_end:%s:%s\r\e[0K\n" "$(date +%s)" "$section_id"
-    if [ "$restore_trace" == 1 ]; then
-       _trace_restore
-    fi
+    _trace_restore
 }
