b8353799ed09ba9d49efa39566bce8e55c0497fe
Tomáš Hozza <thozza@redhat.com>
2026-05-06 19:50:15 +0200

    schutzbot: add scripts and files needed for CI setup
    Add scripts that update GitHub PR with the GitLab CI pipeline status and
    unregister a RHEL host. Also add the file with terraform ref and also
    team SSH keys, which get configured as trusted on the CI runner. These
    files were taken from the osbuild/images and osbuild/osbuild-composer
    repositories and amended for the use in this repo.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

---
+++ schutzbot/update_github_status.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# if a user is logged in to the runner, wait until they're done
+while (( $(who -s | wc -l)  > 0 )); do
+    echo "Waiting for user(s) to log off"
+    sleep 30
+done
+
+if [[ $1 == "start" ]]; then
+  GITHUB_NEW_STATE="pending"
+  GITHUB_NEW_DESC="I'm currently testing this commit, be patient."
+elif [[ $1 == "finish" ]]; then
+  GITHUB_NEW_STATE="success"
+  GITHUB_NEW_DESC="I like this commit!"
+elif [[ $1 == "update" ]]; then
+  if [[ $CI_JOB_STATUS == "canceled" ]]; then
+    GITHUB_NEW_STATE="failure"
+    GITHUB_NEW_DESC="Someone told me to cancel this test run."
+  elif [[ $CI_JOB_STATUS == "failed" ]]; then
+    GITHUB_NEW_STATE="failure"
+    GITHUB_NEW_DESC="I'm sorry, something is odd about this commit."
+  else
+    exit 0
+  fi
+elif [[ $1 == "fail" ]]; then
+    GITHUB_NEW_STATE="failure"
+    GITHUB_NEW_DESC="I'm sorry, something is odd about this commit."
+else
+  echo "unknown command"
+  exit 1
+fi
+
+CONTEXT="Schutzbot on GitLab"
+
+# TODO: This should be amended once we start using schedule triggers
+if [[ "$CI_PIPELINE_SOURCE" == "schedule" ]]; then
+    CONTEXT="$CONTEXT, RHEL-${RHEL_MAJOR:-}-nightly"
+fi
+
+curl \
+    -u "${SCHUTZBOT_LOGIN}" \
+    -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/osbuild/bootc-foundry/statuses/${CI_COMMIT_SHA}" \
+    -d '{"state":"'"${GITHUB_NEW_STATE}"'", "description": "'"${GITHUB_NEW_DESC}"'", "context": "'"${CONTEXT}"'", "target_url": "'"${CI_PIPELINE_URL}"'"}'
